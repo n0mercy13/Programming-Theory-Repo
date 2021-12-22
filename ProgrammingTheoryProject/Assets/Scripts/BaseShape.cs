296b8a87d8ab24caaba2a1a18ade28715348b81c
n0mercy13 <89132411+n0mercy13@users.noreply.github.com>
2021-12-22 13:45:58 +0300

    BaseShape script commit
    - BaseShape class implemented;

--- ProgrammingTheoryProject/Assets/Scripts/BaseShape.cs
+++ ProgrammingTheoryProject/Assets/Scripts/BaseShape.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class BaseShape : MonoBehaviour
 {
+    [SerializeField] protected float moveForce;
     protected string shapeName;
     protected bool isSelected = false;
+    protected bool isMoving = false;
+    
+
+    private Rigidbody shapeRb;
 
     void Start()
     {
-        
+        shapeRb = GetComponent<Rigidbody>();
     }
+
+    public virtual void Greeting()
+    {
+        Debug.Log($"Hello, my name is {shapeName}");        
+    }
+
+    public virtual void Move()
+    {
+        if(isMoving) isMoving = false;
+        if(!isMoving)
+        {
+            isMoving = true;
+            shapeRb.AddForce(Vector3.up * moveForce, ForceMode.Impulse);
+            Debug.Log($"{shapeName} is jumping!");
+        }
+    }
+
+    
 
     
 
