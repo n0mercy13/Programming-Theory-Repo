d546dea494b4a6a9cd7e50db34e0c9b203ee4277
n0mercy13 <89132411+n0mercy13@users.noreply.github.com>
2021-12-22 10:22:56 +0300

    - basic game layout commit
    - game layout;
    - GUI implemented;

---
+++ ProgrammingTheoryProject/Assets/Scripts/BaseShape.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseShape : MonoBehaviour
+{
+    protected string shapeName;
+    protected bool isSelected = false;
+
+    void Start()
+    {
+        
+    }
+
+    
+
+   
+}
